49186912687b880c862147ec9587a361aae72a41
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-30 11:16:00 +0530

    chore: started 30 day react different project building challange to make react concept strong

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between m-4 p-4 bg-green-300 rounded shadow-md">
+      <div>
+        <img
+          className="w-20 h-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/544px-Google_2015_logo.svg.png?20160213081640"
+          alt="logo"
+        />
+      </div>
+      <div>Nav Manu</div>
+    </div>
+  );
+};
+
+export default Header;
