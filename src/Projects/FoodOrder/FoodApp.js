03345cee69d5362ce905064fe14bb979ac163b20
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-06 16:36:26 +0530

    chore:created resturand card

--- src/Projects/FoodOrder/FoodApp.js
+++ src/Projects/FoodOrder/FoodApp.js
@@ -1,5 +1,12 @@
+import ResturantCard from "./ResturantCard";
+
 const FoodApp = () => {
-  return <div>Food App</div>;
+  return (
+    <div className="m-4 p-4 shadow-lg rounded-sm">
+      <h1>Top restaurant chains in Pune</h1>
+      <ResturantCard />
+    </div>
+  );
 };
 
 export default FoodApp;
---
+++ src/Projects/FoodOrder/ResturantCard.js
@@ -0,0 +1,5 @@
+const ResturantCard = () => {
+  return <div>ReasturantCard</div>;
+};
+
+export default ResturantCard;
