6050f8162b5e299b6f4b4f364f40a45150018be2
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-06 16:56:37 +0530

    chore:fetch swii api data

--- src/Projects/FoodOrder/FoodApp.js
+++ src/Projects/FoodOrder/FoodApp.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import ResturantCard from "./ResturantCard";
 
 const FoodApp = () => {
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+  const dataFetch = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
   return (
     <div className="m-4 p-4 shadow-lg rounded-sm">
       <h1>Top restaurant chains in Pune</h1>
