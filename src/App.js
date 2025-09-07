72e219dc19d7fe50ac4dc03fb6f716befbbd85d6
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-07 21:29:53 +0530

    chore: setup path

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Provider } from "react-redux";
 import BGchangeButton from "./Projects/BgChangeBtn/BgchangeBtn";
 import Contactform from "./Projects/Contact/ContactForm";
 import FoodApp from "./Projects/FoodOrder/FoodApp";
+import ResturantPage from "./Projects/FoodOrder/ResturantPage";
 
 export function App() {
   return (
@@ -48,6 +49,10 @@ export const appRouter = createBrowserRouter([
       {
         element: <FoodApp />,
         path: "/foodapp",
+      },
+      {
+        element: <ResturantPage />,
+        path: "/restaurantpage",
       },
     ],
   },
