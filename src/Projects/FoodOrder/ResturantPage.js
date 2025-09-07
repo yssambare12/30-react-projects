6f04361448b21414d6a766d028fc7ab8c545dd75
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-07 21:39:36 +0530

    chore: fetch resturant data

--- src/Projects/FoodOrder/ResturantPage.js
+++ src/Projects/FoodOrder/ResturantPage.js
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 const ResturantPage = () => {
+  useEffect(() => {
+    datafetch();
+  }, []);
+
+  const datafetch = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&restaurantId=151656"
+    );
+    const json = await data.json();
+
+    console.log(
+      json.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[12].card.card
+        .itemCards
+    );
+  };
+
   return (
     <div>
       <h3>BreadCumb -- Home Chhindwara Dev International </h3>
