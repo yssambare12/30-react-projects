03fb1f1e4a4e2e9930924e32b86cdb41bb84ee92
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-10-02 23:07:16 +0530

    chore: added shuffel function

--- src/Projects/QuizApp.js/QuizQuestion.js
+++ src/Projects/QuizApp.js/QuizQuestion.js
@@ -111,3 +111,10 @@ export const data = [
     tags: ["words", "general_knowledge"],
   },
 ];
+
+export const shuffleArray = (array) => {
+  return array
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+};
